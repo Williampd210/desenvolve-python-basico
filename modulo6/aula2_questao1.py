4b7abd7138d646976ad470abdc336f88006128a6
Williampd210 <161207735+Williampd210@users.noreply.github.com>
2025-05-08 09:37:47 -0300

    Update aula2_questao1.py
    Correção da atividade

--- modulo6/aula2_questao1.py
+++ modulo6/aula2_questao1.py
@@ -1,6 +1,8 @@
 import random
 numeros = [random.randint(-100, 100) for _ in range(20)]
-print("Lista ordenada, sem modificar a lista original:" ,(sorted(numeros)))
-print("Lista original:" ,(numeros))
-print("O maior indice é: ",(max(numeros)))
-print("O menor indice é: ",(min(numeros)))
+print("Lista ordenada, sem modificar a lista original:",(sorted(numeros)))
+print("Lista original:",(numeros))
+print("O maior valor é:",(max(numeros)))
+print("E está no indice:",numeros.index(max(numeros)))
+print("O menor valor é:",(min(numeros)))
+print("E está no indice:",numeros.index(min(numeros)))
